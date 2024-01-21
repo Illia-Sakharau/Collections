a67deb614485a53ff3169af8e2257215d4d18571
Illia Sakharau <sakharovia0204@gmail.com>
2024-01-21 15:10:51 +0400

    feat: add internalisation for UsersTable

--- src/components/3-organisms/usersTable/UsersTable.tsx
+++ src/components/3-organisms/usersTable/UsersTable.tsx
@@ -4,39 +4,41 @@ import { createColumnHelper } from '@tanstack/react-table';
 import { DataTable } from '../dataTable/DataTable';
 import { Tag } from '@chakra-ui/react';
 import DateTimeCell from '@molecules/tableCells/DateTimeCell';
+import { useTranslation } from 'react-i18next';
 
 type Props = {
   data: IUserInfo[];
 };
 
 const UsersTable = ({ data }: Props) => {
+  const { t } = useTranslation();
   const columnHelper = createColumnHelper<IUserInfo>();
 
   const columns = [
     columnHelper.accessor('id', {
       cell: (info) => info.getValue(),
-      header: 'ID',
+      header: t('users.id'),
       enableSorting: false,
     }),
     columnHelper.accessor('name', {
       cell: (info) => info.getValue(),
-      header: 'Name',
+      header: t('users.name'),
     }),
     columnHelper.accessor('email', {
       cell: (info) => info.getValue(),
-      header: 'Email',
+      header: t('users.email'),
     }),
     columnHelper.accessor('is_admin', {
       cell: (info) => (info.getValue() ? <Tag>Admin</Tag> : '---'),
-      header: 'Role',
+      header: t('users.role'),
     }),
     columnHelper.accessor('createdAt', {
       cell: (info) => <DateTimeCell stringDate={info.getValue()} />,
-      header: 'Created at',
+      header: t('users.createdAt'),
     }),
     columnHelper.accessor('updatedAt', {
       cell: (info) => <DateTimeCell stringDate={info.getValue()} />,
-      header: 'Updated at',
+      header: t('users.updatedAt'),
     }),
   ];
 
