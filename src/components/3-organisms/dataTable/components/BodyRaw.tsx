e31ad92080dfd58f24d7a405bb7893b41e31dc3e
Illia Sakharau <sakharovia0204@gmail.com>
2024-01-21 14:52:41 +0400

    fix: add hover for BodyRaw

--- src/components/3-organisms/dataTable/components/BodyRaw.tsx
+++ src/components/3-organisms/dataTable/components/BodyRaw.tsx
@@ -1,10 +1,12 @@
-import { Tr } from '@chakra-ui/react';
+import { Tr, useColorModeValue } from '@chakra-ui/react';
 import { Row } from '@tanstack/react-table';
 import BodyCell from './BodyCell';
 
 export default function BodyRaw<Data extends object>({ ...row }: Row<Data>) {
+  const hoverBg = useColorModeValue('gray.300', 'gray.700');
+
   return (
-    <Tr key={row.id}>
+    <Tr key={row.id} _hover={{ bg: hoverBg }}>
       {row.getVisibleCells().map((cell) => (
         <BodyCell {...cell} />
       ))}
