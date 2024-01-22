21e750bfc1e7f00bcc67500ffec807abde987b50
Illia Sakharau <sakharovia0204@gmail.com>
2024-01-22 14:53:38 +0400

    feat: add deleteUsers functionality

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -11,6 +11,22 @@ class usersController {
       res.status(400).json({message: 'Users error'})
     }
   }
+  async deleteUsers (req, res) {
+    try {
+      const { IDs } = req.body;
+      await UsersTB.destroy({
+        where: {
+          id: IDs
+        },
+      });
+      
+      const users = await UsersTB.findAll();
+      res.json(users);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({message: 'Delete error'})
+    }
+  }
 }
 
 export default new usersController();
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -5,5 +5,6 @@ import adminMiddleware from '../middleware/adminMiddleware.js'
 const router = new Router();
 
 router.get('/all', adminMiddleware, userController.getUsers)
+router.delete('/delete', adminMiddleware, userController.deleteUsers)
 
 export default router;
