e81966d20c0da1e97547f77c95d9315044c69042
Illia Sakharau <sakharovia0204@gmail.com>
2024-01-22 21:45:14 +0400

    fix: Users page title i18n

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -3,9 +3,11 @@ import PageLoading from '@/components/1-atoms/PageLoading';
 import { useAppSelector } from '@/hooks/redux';
 import PageHeading from '@atoms/PageHeading';
 import UsersTable from '@organisms/usersTable/UsersTable';
+import { useTranslation } from 'react-i18next';
 
 export default () => {
   useGetAllUsersQuery();
+  const { t } = useTranslation();
   const { isLoading, users, error } = useAppSelector(
     (state) => state.usersListReducer
   );
@@ -16,7 +18,7 @@ export default () => {
 
   return (
     <>
-      <PageHeading pageTitle={'Users'} />
+      <PageHeading pageTitle={t('users.title')} />
       {isLoading && <PageLoading />}
       {users && <UsersTable data={users} />}
       {error && <div>{JSON.stringify(error)}</div>}
