92fd48a504af72f1affdd9cab3ba1553ce0e2c05
Illia Sakharau <sakharovia0204@gmail.com>
2024-01-25 17:59:28 +0400

    feat: add FormSelector component

---
+++ src/components/2-molecules/formFields/FormSelector.tsx
@@ -0,0 +1,47 @@
+import { FC } from 'react';
+import {
+  Select,
+  SelectProps,
+} from '@chakra-ui/react';
+import FieldWrapper, { FieldWrapperProps } from './components/FieldWrapper';
+
+type Props = {
+  options: {
+    value: string;
+    text: string;
+  }[]
+} & SelectProps & Omit<FieldWrapperProps, 'children'>;
+
+const FormSelector: FC<Props> = ({
+  htmlFor,
+  label,
+  isRequired,
+  errorText,
+  helperText,
+  formControlProps,
+  options,
+  ...selectProps
+}) => {
+
+  return (
+    <FieldWrapper 
+      htmlFor={htmlFor}
+      label={label}
+      isRequired={isRequired}
+      errorText={errorText}
+      helperText={helperText}
+      formControlProps={formControlProps}
+    >
+      <Select 
+        id={htmlFor}
+        name={htmlFor}
+        focusBorderColor="brand.500" 
+        {...selectProps}
+      >
+        {options.map((opt)=><option key={opt.value} value={opt.value}>{opt.text}</option>)}
+      </Select>
+    </FieldWrapper>
+  );
+};
+
+export default FormSelector;
